8f377698f7dc12d4d9f8fc1a9180255370d39d00
djumanov <diyorbekjumanov1120@gmail.com>
2023-01-27 16:52:33 +0500

    like and dislike

--- db.py
+++ db.py
@@ -8,13 +8,40 @@ class DB:
                 self.data = json.loads(source)
             else:
                 self.data = {}
-        
 
     def get_data(self):
         return self.data
 
+    def save(self):
+        with open('db.json', 'w') as f:
+            f.write('')
+            f.write(json.dumps(self.data, indent=4))
+
+    def add_user(self, user_id: str):
+        self.data[user_id] = {
+            'like': 0,
+            'dislike': 0
+        }
+        self.save()
+    
+    def increase_like(self, user_id: str):
+        self.data[user_id]['like'] += 1
+        self.save()
+    
+    def increase_dislike(self, user_id: str):
+        self.data[user_id]['dislike'] += 1
+        self.save()
+
+    def get_likes(self, user_id: str) -> dict:
+        return self.data[user_id]
+
 
 db = DB()
+db.increase_like('51514352')
+db.increase_like('51514352')
+db.increase_like('51514352')
+db.increase_like('51514352')
+db.increase_like('51514352')
 print(db.get_data())
-        
+
 
